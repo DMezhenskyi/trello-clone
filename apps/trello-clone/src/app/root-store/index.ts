bd317e0c0cdab12c928198201ea8eefc00da105b
mezhik91 <mezhik91@gmail.com>
2020-01-12 21:40:37 +0100

    add basic redux config

---
+++ apps/trello-clone/src/app/root-store/index.ts
@@ -0,0 +1,10 @@
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { environment } from '../../environments/environment';
+
+export interface AppState {}
+
+export const reducers: ActionReducerMap<AppState> = {};
+
+export const metaReducers: MetaReducer<AppState>[] = !environment.production
+  ? []
+  : [];
--- apps/trello-clone/src/app/root-store/root-store.module.ts
+++ apps/trello-clone/src/app/root-store/root-store.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { environment } from '../../environments/environment';
+import { reducers, metaReducers } from './';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictActionImmutability: true,
+        strictStateImmutability: true
+      }
+    }),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
+  ]
 })
 export class RootStoreModule {}
