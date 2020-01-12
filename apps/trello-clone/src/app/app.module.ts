5acb95c1c83e247602f99acde505b92cd9813663
mezhik91 <mezhik91@gmail.com>
2020-01-12 19:58:57 +0100

    added basic modules

--- apps/trello-clone/src/app/app.module.ts
+++ apps/trello-clone/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { RootStoreModule } from './root-store/root-store.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    RootStoreModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
