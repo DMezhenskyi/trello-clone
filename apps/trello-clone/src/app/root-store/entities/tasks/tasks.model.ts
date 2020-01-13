a6dc0ebfbe9af35b59cc18190fa4bff48e853db0
dme <dme@celum.com>
2020-01-13 11:33:37 +0100

    clean up previous redux state

--- apps/trello-clone/src/app/root-store/entities/tasks/tasks.model.ts
+++
@@ -1,3 +0,0 @@
-export interface Tasks {
-  id: string;
-}
--- apps/trello-clone/src/app/root-store/root-store.module.ts
+++ apps/trello-clone/src/app/root-store/root-store.module.ts
@@ -1,32 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
-import { environment } from '../../environments/environment';
-import { reducers, metaReducers } from './';
-import * as fromTaskList from './entities/task-list/task-list.reducer';
-import * as fromTasks from './entities/tasks/tasks.reducer';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-      runtimeChecks: {
-        strictActionImmutability: true,
-        strictStateImmutability: true
-      }
-    }),
-    EffectsModule.forRoot([]),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
-    StoreModule.forFeature(
-      fromTaskList.taskListsFeatureKey,
-      fromTaskList.reducer
-    ),
-    StoreModule.forFeature(fromTasks.tasksFeatureKey, fromTasks.reducer)
   ]
 })
 export class RootStoreModule {}
